04364cfe41bb7efccd1323bb9085575c1217c992
Matthew Miles <67563275+1eyeITguy@users.noreply.github.com>
2023-07-24 18:07:28 -0500

    Update start-AutopilotOOBE.ps1

--- start-AutopilotOOBE.ps1
+++ start-AutopilotOOBE.ps1
@@ -253,14 +253,8 @@ function Step-oobeSetDeviceRegSettings {
     Write-host -ForegroundColor Yellow "Set Silent Account Configuration"
 
         $HKLMregistryPath = 'HKLM:\SOFTWARE\Policies\Microsoft\OneDrive'##Path to HKLM keys
-        $DiskSizeregistryPath = 'HKLM:\SOFTWARE\Policies\Microsoft\OneDrive\DiskSpaceCheckThresholdMB'##Path to max disk size key
-        $TenantGUID = '8a3a7e59-4ea9-4259-ba0d-77cc328ca84f'
-
         if(!(Test-Path $HKLMregistryPath)){New-Item -Path $HKLMregistryPath -Force}
-        if(!(Test-Path $DiskSizeregistryPath)){New-Item -Path $DiskSizeregistryPath -Force}
-
         New-ItemProperty -Path $HKLMregistryPath -Name 'SilentAccountConfig' -Value '1' -PropertyType DWORD -Force | Out-Null ##Enable silent account configuration
-        New-ItemProperty -Path $DiskSizeregistryPath -Name $TenantGUID -Value '102400' -PropertyType DWORD -Force | Out-Null ##Set max OneDrive threshold before prompting
 
     Write-host -ForegroundColor Yellow "disable firstlogon animation"
 
