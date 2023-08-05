f9be8d232f3fd20ae5bff7288e64d0ed0d1d7be8
Matthew Miles <67563275+1eyeITguy@users.noreply.github.com>
2023-08-05 10:38:06 -0500

    Update Install-OneDrive.ps1

--- Install-OneDrive.ps1
+++ Install-OneDrive.ps1
@@ -154,7 +154,7 @@ function Install-OneDrive {
     Try {
         # Running OneDrive installer
         Write-LogEntry -Value "Starting install OneDrive as per-machine" -Severity 1
-        $OneDriveInstall = Start-Process $SetupFilePath -ArgumentList "/allusers /update" -NoNewWindow -PassThru -ErrorAction Stop
+        $OneDriveInstall = Start-Process $SetupFilePath -ArgumentList "/silent /allusers /update" -Wait -PassThru -ErrorAction Stop
     } catch [System.Exception] {
         Write-LogEntry -Value "Error installing OneDrive as per-machine. ErrorMessage: $($_.Exception.Message)" -Severity 3
     }
